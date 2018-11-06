1d3a4865377b607f88b3d40d17192700603735a7
mattosaurus <mattosaurus@outlook.com>
2018-11-06 11:11:10 +0000

    Resize blob images

---
+++ ImageViewer/AppCode/Common.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageViewer.AppCode
+{
+    public static class Common
+    {
+        public static async Task ResizeImage(Stream stream, CloudBlockBlob destinationBlob)
+        {
+            var image = SixLabors.ImageSharp.Image.Load(stream);
+            var width = 2000;
+            var height = 2000 * image.Height / image.Width;
+
+            var output = new MemoryStream();
+            image.Clone(i =>
+                i.Resize(width, height, KnownResamplers.Bicubic))
+                .SaveAsJpeg(output);
+
+            output.Seek(0, SeekOrigin.Begin);
+
+            // Save to blob
+            await destinationBlob.UploadFromStreamAsync(output);
+        }
+    }
+}
--- ImageViewer/Controllers/HomeController.cs
+++ ImageViewer/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using ImageViewer.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.Extensions.Configuration;
+using ImageViewer.AppCode;
 
 namespace ImageViewer.Controllers
 {
@@ -36,12 +37,25 @@ namespace ImageViewer.Controllers
 
             do
             {
-                BlobResultSegment results = await container.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                BlobResultSegment results = await container.ListBlobsSegmentedAsync("raw/", blobContinuationToken);
                 // Get the value of the continuation token returned by the listing call.
                 blobContinuationToken = results.ContinuationToken;
                 // Cast results to CloudBlockBlob
-                blobNames.AddRange(results.Results.OfType<CloudBlockBlob>().Select(b => b.Name).ToList());
+                blobNames.AddRange(results.Results.OfType<CloudBlockBlob>().Select(b => b.Name.Replace("raw/", "")).ToList());
             } while (blobContinuationToken != null); // Loop while the continuation token is not null.
+
+            //foreach (string blobName in blobNames)
+            //{
+            //    CloudBlockBlob rawBlob = container.GetBlockBlobReference("raw/" + blobName);
+            //    CloudBlockBlob webBlob = container.GetBlockBlobReference("web/" + blobName);
+            //    webBlob.Properties.ContentType = "image/jpeg";
+
+            //    MemoryStream memoryStream = new MemoryStream();
+            //    await rawBlob.DownloadToStreamAsync(memoryStream);
+            //    memoryStream.Seek(0, SeekOrigin.Begin);
+
+            //    await Common.ResizeImage(memoryStream, webBlob);
+            //}
 
             blobNames.Shuffle();
 
@@ -53,7 +67,7 @@ namespace ImageViewer.Controllers
         {
             // Retrieve a reference to a container.
             CloudBlobContainer container = new CloudBlobContainer(new Uri(_config["AzureStorage:ContainerConnectionString"]));
-            CloudBlockBlob blob = container.GetBlockBlobReference(id);
+            CloudBlockBlob blob = container.GetBlockBlobReference("web/" + id);
 
             MemoryStream memoryStream = new MemoryStream();
             await blob.DownloadToStreamAsync(memoryStream);
